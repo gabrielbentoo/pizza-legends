23587887f5ac12431a070cc07c6bf5afa137cec5
Gabriel Bento <bielnovaes61@gmail.com>
2026-09-21 16:46:17 -0300

    adicao das classes GameObject e Sprite

--- Overworld.js
+++ Overworld.js
@@ -12,6 +12,23 @@ class Overworld {
             this.ctx.drawImage(image, 0, 0);
         };
         image.src = "images/maps/DemoLower.png";
+
+        
+        const hero = new GameObject({
+            x: 5,
+            y: 6,
+        });
+        
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "images/characters/people/npc1.png"
+        })
+
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200)
     }
 
 }
